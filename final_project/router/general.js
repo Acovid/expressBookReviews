6578faf3dcd9a810ab0f2f295ce965af2de3edde
Aco Vidovic <acovid@gmail.com>
2025-03-26 08:33:13 -0400

    finish task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,8 +18,13 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn; // Extract ISBN from URL parameter
+
+    if (books[isbn]) {
+        return res.status(200).json(books[isbn]); // Return book details
+    } else {
+        return res.status(404).json({ message: "Book not found" }); // If ISBN not found
+    }
  });
   
 // Get book details based on author
